14fee9afb4274b3614ad8f3e182d0deb8e22c2bf
johncode101 <john.carrera911@gmail.com>
2021-11-30 23:54:42 -0800

    Buttons were added and code was refactored for better read.

--- Dating app/ViewController.swift
+++ Dating app/ViewController.swift
@@ -8,41 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let topStackView = TopNavigationStackView()
+    let blueView = UIView()
+    let buttonsStackView = HomeBottomControlsStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let grayView = UIView()
-        grayView.backgroundColor = .gray
-        
-        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
-            let v = UIView()
-            v.backgroundColor = color
-            return v
-        }
-        
-        let topStackView = UIStackView(arrangedSubviews: subviews)
-        topStackView.distribution = .fillEqually
-        
-        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        let blueView = UIView()
-        blueView.backgroundColor = .blue
-        let yellowView = UIView()
-        yellowView.backgroundColor = .yellow
-        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
-        
-        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
-//        stackView.distribution = .fillEqually
-        stackView.axis = .vertical
-        
-        
-        view.addSubview(stackView)
-        stackView.frame = .init(x: 0, y: 0, width: 300, height: 200)
-        
-        stackView.fillSuperview()
-        
+        blueView.backgroundColor = .systemPink
+        setupLayout()
     }
 
+    // MARK:- Fileprivate
+    
+    fileprivate func setupLayout() {
+        let overallStackView = UIStackView(arrangedSubviews: [topStackView, blueView, buttonsStackView])
+        overallStackView.axis = .vertical
+        overallStackView.backgroundColor = .systemBackground
+        view.addSubview(overallStackView)
+        overallStackView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+    }
 
 }
