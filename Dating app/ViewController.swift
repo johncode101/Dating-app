c1bd14d6d9c365b455eededbcb4386b22938800a
johncode101 <john.carrera911@gmail.com>
2021-12-02 01:06:00 -0800

    Image and animation swipe were added.

--- Dating app/ViewController.swift
+++ Dating app/ViewController.swift
@@ -5,29 +5,39 @@
 //  Created by Jonathan Hernandez on 11/29/21.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
     
     let topStackView = TopNavigationStackView()
-    let blueView = UIView()
+    let cardsDeckView = UIView()
     let buttonsStackView = HomeBottomControlsStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        blueView.backgroundColor = .systemPink
         setupLayout()
+        setupDummyCards()
+    }
+    
+    fileprivate func setupDummyCards() {
+        let cardView = CardView(frame: .zero)
+        cardsDeckView.addSubview(cardView)
+        cardView.fillSuperview()
     }
 
     // MARK:- Fileprivate
     
     fileprivate func setupLayout() {
-        let overallStackView = UIStackView(arrangedSubviews: [topStackView, blueView, buttonsStackView])
+        let overallStackView = UIStackView(arrangedSubviews: [topStackView, cardsDeckView, buttonsStackView])
         overallStackView.axis = .vertical
         overallStackView.backgroundColor = .systemBackground
         view.addSubview(overallStackView)
         overallStackView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        overallStackView.isLayoutMarginsRelativeArrangement = true
+        overallStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        
+        overallStackView.bringSubviewToFront(cardsDeckView)
     }
 
 }
